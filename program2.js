7d24b1976c2e297dd6d48838ae8bd39297a920a9
bachu8691 <vishruth.ravikanti@gmail.com>
2024-11-11 13:34:27 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,32 @@
 const decodeTheRing = function (s, p) {
+  const m = s.length;
+  const n = p.length;
+  const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
 
-    // write your code here
+  // Base case: empty pattern matches empty string
+  dp[0][0] = true;
 
-  };
-  
-  module.exports = decodeTheRing;
+  // Fill the first row for cases where pattern starts with '*'
+  for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === '*') {
+          dp[0][j] = dp[0][j - 1];
+      }
+  }
+
+  // Fill the DP table
+  for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+          if (p[j - 1] === '*') {
+              dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+          } else if (p[j - 1] === '?') {
+              dp[i][j] = dp[i - 1][j - 1];
+          } else {
+              dp[i][j] = dp[i - 1][j - 1] && s[i - 1] === p[j - 1];
+          }
+      }
+  }
+
+  return dp[m][n];
+};
+
+module.exports = decodeTheRing;
