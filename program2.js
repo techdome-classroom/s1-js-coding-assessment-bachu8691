227d20da16d4ac71e61da5a7f2535fe1b0c5c694
bachu8691 <vishruth.ravikanti@gmail.com>
2024-11-11 13:56:51 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,29 +1,35 @@
 const decodeTheRing = function (s, p) {
-  const m = s.length;
-  const n = p.length;
-  const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
+    const m = s.length;
+    const n = p.length;
+    const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
 
-  dp[0][0] = true;
+    // Base case: empty pattern matches empty string
+    dp[0][0] = true;
 
-  for (let j = 1; j <= n; j++) {
-      if (p[j - 1] === '*') {
-          dp[0][j] = dp[0][j - 1];
-      }
-  }
+    // Initialize dp for patterns that begin with '*'
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
 
-  for (let i = 1; i <= m; i++) {
-      for (let j = 1; j <= n; j++) {
-          if (p[j - 1] === '*') {
-              dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
-          } else if (p[j - 1] === '?') {
-              dp[i][j] = dp[i - 1][j - 1];
-          } else {
-              dp[i][j] = dp[i - 1][j - 1] && s[i - 1] === p[j - 1];
-          }
-      }
-  }
+    // Fill the DP table
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (p[j - 1] === '*') {
+                // '*' matches zero or more characters
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            } else if (p[j - 1] === '?') {
+                // '?' matches exactly one character
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                // Regular character match
+                dp[i][j] = dp[i - 1][j - 1] && s[i - 1] === p[j - 1];
+            }
+        }
+    }
 
-  return dp[m][n];
+    return dp[m][n];
 };
 
 module.exports = decodeTheRing;
